9e418ba9d9956897cf914a807bc210ebd23f82f3
Hafiz Halim <hafizz08@gmail.com>
2011-02-22 20:56:58 +0800

    Button helper

--- lib/jqmobile_helpers/buttons_helper.rb
+++ lib/jqmobile_helpers/buttons_helper.rb
@@ -1,7 +1,26 @@
 module JqmobileHelpers
   # = JqmobileHelpers Buttons Helper
-  # Provides a set of helper methods for jquery-mobile buttons 
+  # Provides a set of helper methods for jquery-mobile buttons
   module ButtonsHelper #:nodoc:
-    
+
+    # Button Link
+    #
+    # ==== Examples
+    #   button_link 'New Post', new_post_path
+    #   # => <a data-role="button" href="/posts/new" data-theme="c">New Post</a>
+    #
+    def button_link(name, link, options = {})
+
+      html_options = options.stringify_keys!
+      default_options = {'data-role' => "button"}
+
+      if html_options.has_key?('data-icon')
+        default_options = default_options.merge({'data-icon' => html_options['data-icon']})
+      end
+
+      content_tag(:a, name, :href => link, default_options)
+    end
+
   end
 end
+
