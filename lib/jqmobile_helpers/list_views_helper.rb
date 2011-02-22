b556d51bfe721f0d1bb3f3e3b878aed48bb106b2
Fadhli Rahim <fadhlirahim@gmail.com>
2011-02-22 18:40:41 +0800

    Create numbered list

--- lib/jqmobile_helpers/list_views_helper.rb
+++ lib/jqmobile_helpers/list_views_helper.rb
@@ -3,6 +3,9 @@ module JqmobileHelpers
   # Provides a set of methods for making list views 
   # for jquery-mobile markup
   module ListViewsHelper 
+    # html5 data attributes options value
+    mattr_accessor :default_options 
+    
     # Creates a simple unordered list containing linked list items 
     # with a data-role="listview" attribute
     # 
@@ -20,20 +23,51 @@ module JqmobileHelpers
     #
     def basic_list(collection, options = {})
       
-      html_options = options.stringify_keys! 
-      default_options = {'data-role' => "listview", 'data-inset' => "true"}
-      
-      if html_options.has_key?('data-inset')
-        default_options = default_options.merge({'data-inset' => html_options['data-inset']})
-      end
-      
-      if html_options.has_key?('data-theme')
-        default_options = default_options.merge({'data-theme' => html_options['data-theme']})
-      end
+      html_attributes_options(options)
           
       list = collection.map {|item| content_tag("li", item)}
       content_tag(:ul, list.join.html_safe, default_options)
     end
     
+    # Creates ordered lists (ol) which is useful when presented items that are in a sequence.
+    # When the enhanced markup is applied to the list view, jQuery Mobile will try to first use CSS to add numbers 
+    # to the list and, if not supported, will fall back to injecting numbers with JavaScript
+    # 
+    # ==== Options
+    #   # => 'data-inset' => 'true' (Default data-inset is set to true)
+    #   # => 'data-theme' => 'c' (Default data-theme is set to c)
+    #      
+    # ==== Examples
+    #   basic_list(@posts.map{|x| x.title})
+    #   # => <ol data-role="listview" data-inset="true"><li>Title 1</li><li>test2</li></ol> 
+    #
+    #   basic_list(@posts.map{|x| link_to(x.title, post_path(x))}, {'data-inset'=>'false', 'data-theme'=>'b'})
+    #   # => <ol data-role="listview" data-inset="false" data-theme="b"> \\
+    #          <li><a href='/posts/1'>Title 1</a></li><li><a href="/posts/2">test2</a></li></ol> 
+    #
+    def numbered_list(collection, options = {})
+      html_attributes_options(options)
+      
+      list = collection.map {|item| content_tag("li", item)}
+      content_tag(:ol, list.join.html_safe, self.default_options)
+    end
+    
+    
+    private
+      
+      # Default html5 data attributes for list view in jquery-mobile
+      def html_attributes_options(options)
+        html_options = options.stringify_keys! 
+        self.default_options = {'data-role' => "listview", 'data-inset' => "true"}
+
+        if html_options.has_key?('data-inset')
+          self.default_options = default_options.merge({'data-inset' => html_options['data-inset']})
+        end
+
+        if html_options.has_key?('data-theme')
+          self.default_options = default_options.merge({'data-theme' => html_options['data-theme']})
+        end
+      end
+      
   end
 end
--- test/list_views_helper_test.rb
+++ test/list_views_helper_test.rb
@@ -13,4 +13,9 @@ class ListViewHelperTest < ActiveSupport::TestCase
     basic_list = basic_list(collection.map{|x| link_to(x[:name], x[:link])}, {'data-theme' => 'b'})
     assert_equal "<ul data-inset=\"true\" data-role=\"listview\" data-theme=\"b\"><li><a href=\"http://google.com/serach?q=Cs8\">Cs8</a></li></ul>", basic_list
   end
+  
+  test "basic numbered list helper" do
+    jq_basic_list = numbered_list(["apple", "orange"])
+    assert_equal "<ol data-inset=\"true\" data-role=\"listview\"><li>apple</li><li>orange</li></ol>", jq_basic_list
+  end
 end
