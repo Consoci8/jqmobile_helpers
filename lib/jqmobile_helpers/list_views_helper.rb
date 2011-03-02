e25023c44b903645b9aa2f89ff153355aca702a5
Haikal Hussein <haikalhussein@Muhammad-Haikal-Mohd-Husseins-MacBook-Pro.local>
2011-03-02 12:19:12 +0800

    fixed merge conflicts

--- lib/jqmobile_helpers/list_views_helper.rb
+++ lib/jqmobile_helpers/list_views_helper.rb
@@ -220,6 +220,51 @@ module JqmobileHelpers
         content_tag(:ul, list.join.html_safe, default_options.update('data-filter' => 'true', 'data-inset' => 'false'))
       end
     
+      # ====================================== LIST FORMATTING LIST ===========================================================
+      # jQuery Mobile provides a very easy way to filter a list with a simple client-side search feature. 
+      # To make a list filterable, simply add the data-filter="true" attribute to the list. 
+      # The framework will then append a search box above the list 
+      # and add the behavior to filter out list items that don't contain the current search string as the user types.
+      #
+      # ==== Examples
+      #      <%= search_filter_list(@posts.map{|x| link_to(x.title, post_path(x))}) %> 
+      #      # => <ul data-inset="false" data-role="listview" data-filter="true">
+      #             <li>          
+      #               <a href="/posts/1">Title 1</a></h3><p>Title 1</p>
+      #             </li>
+      #           </ul> 
+      #
+      def list_formatting(collection, options = {})
+        html_attributes_options(options)
+        #html_li_attributes_options(options)
+        list = collection.map do |item|
+            if item.is_a?(Array)
+                content_tag("<li data-role=list-divider>#{item[0]}<span class=ui-li-count>#{item[1]}</span>", content_tag("li", item))
+            end
+          end
+        content_tag(:ul, list, self.default_options)
+      end
+      
+      # ====================================== LIST DIVIDER ===========================================================
+      # jQuery Mobile provides a very easy way to filter a list with a simple client-side search feature. 
+      # To make a list filterable, simply add the data-filter="true" attribute to the list. 
+      # The framework will then append a search box above the list 
+      # and add the behavior to filter out list items that don't contain the current search string as the user types.
+      #
+      # ==== Examples
+      #      <%= search_filter_list(@posts.map{|x| link_to(x.title, post_path(x))}) %> 
+      #      # => <ul data-inset="false" data-role="listview" data-filter="true">
+      #             <li>          
+      #               <a href="/posts/1">Title 1</a></h3><p>Title 1</p>
+      #             </li>
+      #           </ul> 
+      #
+      def list_divider(collection, options = {})
+        html_attributes_options(options)
+        #html_li_attributes_options(options)
+        list = collection.map{|item| content_tag(:li, content_tag("li", item), {'data-role' => 'list-divider', 'span' => 'ui-li-count'})}
+        content_tag(:ul, list.join.html_safe, self.default_options)
+      end
     private
       
       # Default html5 data attributes for list view in jquery-mobile
@@ -242,8 +287,12 @@ module JqmobileHelpers
         if html_options.has_key?('data-transition')
           self.default_options = default_options.merge({'data-transition' => html_options['data-transition']})
         end
-        
-        
+  
+      end
+      
+      def html_li_attributes_options(options)
+        html_options = options.stringify_keys!
+        self.li_options = {'data-role' => "list-divider"}
       end
 
   end
